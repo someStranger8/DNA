e45b49b9176ad3a1ea5599f1e460b3b297a0ac62
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-12-11 11:53:50 -0500

    Update dna.py

--- dna.py
+++ dna.py
@@ -2,7 +2,7 @@ import random
 nucleobases = ["G","A","T","C"]
 dna_file = open("dna.txt", "x")
 
-for x in range(3000000000):
+for x in range(1000):
     base_pair = str.join(random.choice(nucleobases), random.choice(nucleobases))
     dna_file.write(base_pair)
     
